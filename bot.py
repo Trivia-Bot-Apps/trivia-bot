ced2e03935e5b4f065ad7081f8a24a0aef2aaa62
Evan Gubarev <evan@evangubarev.com>
2020-11-01 16:40:40 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ from discord.ext.commands import (
     has_permissions,
     MissingPermissions,
     AutoShardedBot,
-    CommandOnCooldown
+    CommandOnCooldown,
 )
 from discord.ext import commands, tasks
 from discord.utils import find
@@ -60,6 +60,17 @@ from profanityfilter import ProfanityFilter
 import sentry_sdk
 import homoglyphs as hg
 from googletrans import Translator
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
 
 pf = ProfanityFilter()
 
